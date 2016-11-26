c8012206b721aff5925d04c6495b59f51933d8c7
SandPox <sandpox@protonmail.ch>
2016-11-26 12:36:24 +0000

    js01ltedcm: Build consumerir
    * OSS FTW.
    
    Conflicts:
    	proprietary-files.txt

--- device.mk
+++ device.mk
@@ -132,6 +132,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.msm8974
 
+# IR Blaster
+PRODUCT_PACKAGES += \
+    consumerir.msm8974
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
